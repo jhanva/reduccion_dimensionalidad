020defe0dd888ba47f3ff743708926bb12617696
jhanva <jarbey.mejia@gmail.com>
2023-09-04 10:12:26 -0500

    feat: calculo matricial

--- machine_learning_ii/taller_1/1.matrix.py
+++ machine_learning_ii/taller_1/1.matrix.py
@@ -1,4 +1,19 @@
+# External libraries
+from numpy.linalg import det
+
+from machine_learning_ii.utils import algebra as al
 
 
 if __name__ == '__main__':
-    print(create_matrix(4, 6))
+
+    matrix = al.create_matrix(4, 4)
+
+    rank = al.rank_matrix(matrix)
+    trace = al.trace_matrix(matrix)
+    determinant = al.determinant_matrix(matrix)
+
+    print(determinant == round(det(matrix)))
+
+    print(f'Rank of matrix: {rank}')
+    print(f'Trace of matrix: {trace}')
+    print(f'Determinant of matrix: {determinant}')
