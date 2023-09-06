08ff231cb9b35a96d2021d8bed2975d0c21050e6
jhanva <jarbey.mejia@gmail.com>
2023-09-05 23:56:36 -0500

    feat: modulo del punto 4

---
+++ machine_learning_ii/taller_1/4.svd_picture.py
@@ -0,0 +1,26 @@
+# External libraries
+import matplotlib.pyplot as plt
+from skimage import io
+from unsupervised.python.dimensionality_reduction import SVD
+
+# Own libraries
+from machine_learning_ii.metadata.path import Path
+
+if __name__ == '__main__':
+    picture = io.imread(Path.img_johan_transform)
+
+    fig, axes = plt.subplots(2, 5, figsize=(12, 6))
+
+    for j, k in enumerate(range(2, 22, 2)):
+        svd = SVD(n_components=k)
+        matrix = svd.fit_transform(picture)
+
+        row = j // 5
+        col = j % 5
+
+        axes[row, col].imshow(matrix, cmap='gray')
+        axes[row, col].axis('off')
+        axes[row, col].set_title(f'n_components: {k}')
+
+    plt.tight_layout()
+    plt.show()
