26ff1b906349d78201dcf8209bb442ca92915d09
jhanva <jarbey.mejia@gmail.com>
2023-09-06 22:53:48 -0500

    docs: agrega comentarios

--- python/homework/5.mnist.py
+++ python/homework/5.mnist.py
@@ -2,5 +2,8 @@
 from python.utils.regression import mnist_logistic_regression
 
 if __name__ == '__main__':
+    # Model without processing
     mnist_logistic_regression()
+
+    # Model with processing
     mnist_logistic_regression(True)
